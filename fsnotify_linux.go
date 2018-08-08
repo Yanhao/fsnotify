c70202c3e61e227387e4e42c41e15bdfc87cb8d3
Yanhao Mo <yanhaocs@gmail.com>
2018-08-08 17:26:11 +0800

    export FileEvent 's Mask filed

--- fsnotify_linux.go
+++ fsnotify_linux.go
@@ -57,34 +57,34 @@ const (
 )
 
 type FileEvent struct {
-	mask   uint32 // Mask of events
+	Mask   uint32 // Mask of events
 	cookie uint32 // Unique cookie associating related events (for rename(2))
 	Name   string // File name (optional)
 }
 
 // IsCreate reports whether the FileEvent was triggered by a creation
 func (e *FileEvent) IsCreate() bool {
-	return (e.mask&sys_IN_CREATE) == sys_IN_CREATE || (e.mask&sys_IN_MOVED_TO) == sys_IN_MOVED_TO
+	return (e.Mask&sys_IN_CREATE) == sys_IN_CREATE || (e.Mask&sys_IN_MOVED_TO) == sys_IN_MOVED_TO
 }
 
 // IsDelete reports whether the FileEvent was triggered by a delete
 func (e *FileEvent) IsDelete() bool {
-	return (e.mask&sys_IN_DELETE_SELF) == sys_IN_DELETE_SELF || (e.mask&sys_IN_DELETE) == sys_IN_DELETE
+	return (e.Mask&sys_IN_DELETE_SELF) == sys_IN_DELETE_SELF || (e.Mask&sys_IN_DELETE) == sys_IN_DELETE
 }
 
 // IsModify reports whether the FileEvent was triggered by a file modification or attribute change
 func (e *FileEvent) IsModify() bool {
-	return ((e.mask&sys_IN_MODIFY) == sys_IN_MODIFY || (e.mask&sys_IN_ATTRIB) == sys_IN_ATTRIB)
+	return ((e.Mask&sys_IN_MODIFY) == sys_IN_MODIFY || (e.Mask&sys_IN_ATTRIB) == sys_IN_ATTRIB)
 }
 
 // IsRename reports whether the FileEvent was triggered by a change name
 func (e *FileEvent) IsRename() bool {
-	return ((e.mask&sys_IN_MOVE_SELF) == sys_IN_MOVE_SELF || (e.mask&sys_IN_MOVED_FROM) == sys_IN_MOVED_FROM)
+	return ((e.Mask&sys_IN_MOVE_SELF) == sys_IN_MOVE_SELF || (e.Mask&sys_IN_MOVED_FROM) == sys_IN_MOVED_FROM)
 }
 
 // IsAttrib reports whether the FileEvent was triggered by a change in the file metadata.
 func (e *FileEvent) IsAttrib() bool {
-	return (e.mask & sys_IN_ATTRIB) == sys_IN_ATTRIB
+	return (e.Mask & sys_IN_ATTRIB) == sys_IN_ATTRIB
 }
 
 type watch struct {
@@ -242,7 +242,7 @@ func (w *Watcher) readEvents() {
 			// Point "raw" to the event in the buffer
 			raw := (*syscall.InotifyEvent)(unsafe.Pointer(&buf[offset]))
 			event := new(FileEvent)
-			event.mask = uint32(raw.Mask)
+			event.Mask = uint32(raw.Mask)
 			event.cookie = uint32(raw.Cookie)
 			nameLen := uint32(raw.Len)
 			// If the event happened to the watched directory or the watched file, the kernel
@@ -287,7 +287,7 @@ func (w *Watcher) readEvents() {
 // against files that do not exist.
 func (e *FileEvent) ignoreLinux() bool {
 	// Ignore anything the inotify API says to ignore
-	if e.mask&sys_IN_IGNORED == sys_IN_IGNORED {
+	if e.Mask&sys_IN_IGNORED == sys_IN_IGNORED {
 		return true
 	}
 
